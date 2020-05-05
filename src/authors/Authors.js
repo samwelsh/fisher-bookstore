c26cd9b5cdf87fa09d508310faec3a2e1b503217
Sam Welsh <welsh.271@osu.edu>
2020-05-05 17:53:40 -0400

    fisher-bookstore

--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -1,43 +1,23 @@
-import React, { Component } from "react";
-import "./Authors.css";
-import { AuthorsDisplay } from "./AuthorsDisplay";
 
-export default class Authors extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            authors: [
-                {
-                    id: 1,
-                    fName: "JK",
-                    lName: "Rowling",
-                    numTitles: 14,
-                    image: "https://upload.wikimedia.org/wikipedia/commons/5/5d/J._K._Rowling_2010.jpg" ,
-                },
-                {
-                    id: 2,
-                    fName: "Barack",
-                    lName: "Obama",
-                    numTitles: 2,
-                    image: "https://upload.wikimedia.org/wikipedia/commons/8/8d/President_Barack_Obama.jpg",
-                },
-                {
-                    id: 3,
-                    fName: "Stephen",
-                    lName: "King",
-                    numTitles: 31,
-                    image: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Stephen_King%2C_Comicon.jpg",
-                }
-            ] 
-        }
-    }
-    render() {
-        return (
-            <div className="Authors">
-                <div className="lander">
-                    <AuthorsDisplay authors={this.state.authors} />
-               </div>
+import "./Authors.css";
+
+import React, {useEffect, useState } from "react";
+import {AuthorsDisplay} from "./AuthorsDisplay";
+
+export default function Authors(props) {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://localhost:5001/api/authors/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+    
+    return (
+        <div className="Authors">
+            <div className="lander">
+                <AuthorsDisplay authors={data} />
             </div>
-        );
-    }
+        </div>
+    )
 }
--- src/books/Books.js
+++ src/books/Books.js
@@ -1,82 +1,24 @@
-import React, { Component } from "react";
-import { BookDisplay } from "./BookDisplay";
-import "./Books.css";
 
- /* export default function Books () {
+//import "./Books.css";
+
+import React, {useEffect, useState } from "react";
+import { BookDisplay } from "./BookDisplay";
+
+export default function Books(props) {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://localhost:5001/api/books/")
+            .then(response => response.json())
+            .then(data => setData(data));      
+    }, []);
+    
     return (
         <div className="Books">
             <div className="lander">
-                <h1>Books</h1>
-                <p>Books Section.</p>
+                <BookDisplay books={data} />
             </div>
         </div>
     );
-} */
-
-export default class Books extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            books: [
-                {
-                    id: 1,
-                    title: "Domain Driven Design",
-                    author: "Eric Evans",
-                    isbn: "978-0321125217"
-                } ,
-                {
-                    id: 2,
-                    title: "Accelerate",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                } ,
-                {
-                    id: 3,
-                    title: "Financial Accounting",
-                    author: "Marc Smith",
-                    isbn: "823-7281507965"
-                } ,
-                {
-                    id: 4,
-                    title: "Managerial Accounting",
-                    author: "Marc Smith",
-                    isbn: "412-7604651320"
-                } ,
-                {
-                    id: 5,
-                    title: "If Animals Kissed Goodnight",
-                    author: "Ann Whitford Paul",
-                    isbn: "985-8905753215"
-                } ,
-                {
-                    id: 6,
-                    title: "Open Book",
-                    author: "Jessica Simpson",
-                    isbn: "369-1836383985"
-                } ,
-                {
-                    id: 7,
-                    title: "The Very Hungry Caterpillar",
-                    author: "Eric Carle",
-                    isbn: "102-0702149648"
-                } ,
-                {
-                    id: 8,
-                    title: "Becoming",
-                    author: "Michelle Obama",
-                    isbn: "436-6319562018"
-                }
-            ]
-        }
-    };
-    render() {
-        return (
-            <div className="Books">
-                <div className="lander">
-                    <BookDisplay books={this.state.books} />
-               </div>
-            </div>
-        );
-    }
 }
 
